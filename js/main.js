94db09af63a542c7350a55dd8c60c37eedc2fc35
unknown <html@2-00.itfactory.neta>
2015-10-05 18:12:54 +0200

    Alap téma és termékek

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// Fő angular modul.
+var cipobolt = angular.module("cipobolt", []);
+
+// Termékek.
+cipobolt.controller("termekCtrl", function ($scope) {
+
+    // Termékek listája.
+    $scope.termekek = [
+        {
+            "name": "Piros cipő",
+            "price": "11000",
+            "img": "http://keptaram.hu/kt/7/3451/rAPGoCGpduvPbCxVKPvn/piros-szaten-cipo.jpg"
+        },
+        {
+            "name": "Zöld cipő",
+            "price": "11000",
+            "img": "http://keptaram.hu/kt/7/3451/rAPGoCGpduvPbCxVKPvn/piros-szaten-cipo.jpg"
+        },
+        {
+            "name": "Kék cipő",
+            "price": "11000",
+            "img": "http://keptaram.hu/kt/7/3451/rAPGoCGpduvPbCxVKPvn/piros-szaten-cipo.jpg"
+        },
+        {
+            "name": "Fekete cipő",
+            "price": "11000",
+            "img": "http://keptaram.hu/kt/7/3451/rAPGoCGpduvPbCxVKPvn/piros-szaten-cipo.jpg"
+        }
+    ];
+
+});
+
+// Téma módosítása.
+$("#theme-selector a").click(function (e) {
+
+    // Esemény megállítása.
+    e.preventDefault();
+
+    // A kiválasztott link url-je.
+    var theme = this.href.replace(location.origin, "").replace(/\//g, "");
+
+    // Fő css link keresése és cseréje.
+    // lib/bootswatch/cerulean/bootstrap.css
+    var mainCss = $("#main-css");
+    var href = mainCss.attr("href");
+    href = href.replace(/bootswatch\/.*\/bootstrap/, "bootswatch/" + theme + "/bootstrap");
+    mainCss.attr("href", href);
+
+});
